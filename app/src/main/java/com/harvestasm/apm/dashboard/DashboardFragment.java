35242796ac43b251e0c99f451d0b66d273727160
yangfeng <yangfeng@sogou-inc.com>
2018-04-16 20:23:41 +0800

    unify the swip to refersh fragment and its loading progress with MVVM

--- app/src/main/java/com/harvestasm/apm/dashboard/DashboardFragment.java
+++ app/src/main/java/com/harvestasm/apm/dashboard/DashboardFragment.java
@@ -1,13 +1,10 @@
 package com.harvestasm.apm.dashboard;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.harvestasm.apm.sample.R;
@@ -19,52 +16,29 @@ import java.util.List;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DashboardFragment extends BaseSwipeRefreshFragment {
+public class DashboardFragment extends BaseSwipeRefreshFragment<ChartItem, ListView> {
     private static final String TAG = DashboardFragment.class.getSimpleName();
 
     private DashboardViewModel viewMultiChartModel;
-
-    private ListView lv;
     private DashboardAdapter cda;
 
-    public DashboardFragment() {
+    @Override
+    protected @LayoutRes int getCollectionLayoutResourceId() {
+        return R.layout.including_listview;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_multichart, container, false);
-        lv = view.findViewById(R.id.listView1);
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        setViewModel();
-        startLoading(viewMultiChartModel.refreshState);
+    protected void refreshChangedData(ListView lv, List<ChartItem> chartItems) {
+        cda = new DashboardAdapter(getContext(), chartItems, viewMultiChartModel);
+        lv.setAdapter(cda);
     }
 
     protected void doLoadingTask() {
         viewMultiChartModel.load(getTypeface());
     }
 
-    private void setViewModel() {
-        viewMultiChartModel = getViewModel();
-
-        viewMultiChartModel.items.observe(this, new Observer<List<ChartItem>>() {
-            @Override
-            public void onChanged(@Nullable List<ChartItem> chartItems) {
-                if (null == chartItems) {
-                    Log.w(TAG, "null data comes.");
-                } else {
-                    Log.d(TAG, "data size " + chartItems.size());
-                    cda = new DashboardAdapter(getContext(), chartItems, viewMultiChartModel);
-                    lv.setAdapter(cda);
-                }
-            }
-        });
+    @Override
+    protected LiveData<List<ChartItem>> setViewModel() {
+        viewMultiChartModel = newViewModel(DashboardViewModel.class);
 
         viewMultiChartModel.clickItem.observe(this, new Observer<ChartItem>() {
             @Override
@@ -82,6 +56,10 @@ public class DashboardFragment extends BaseSwipeRefreshFragment {
                 }
             }
         });
+
+        startLoading(viewMultiChartModel.refreshState);
+
+        return viewMultiChartModel.items;
     }
 
     private void onClickById(@Nullable ChartItem item) {
@@ -117,10 +95,5 @@ public class DashboardFragment extends BaseSwipeRefreshFragment {
         } else {
             Log.e(TAG, "Unknown item id: " + id);
         }
-    }
-
-    private DashboardViewModel getViewModel() {
-        return ViewModelProviders.of(this/*, new MultiChartViewModelFactory()*/)
-                .get(DashboardViewModel.class);
     }
 }
--- app/src/main/java/com/harvestasm/apm/home/HomeFragment.java
+++ app/src/main/java/com/harvestasm/apm/home/HomeFragment.java
@@ -1,75 +1,46 @@
 package com.harvestasm.apm.home;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.arch.lifecycle.LiveData;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.harvestasm.apm.sample.R;
 import com.harvestasm.base.viewholder.BaseSwipeRefreshFragment;
 
 import java.util.List;
 
-/**
- * A placeholder fragment containing a simple view.
+/***
+ * 首页数据显示，维护管理和RecyclerView对应的ViewModel, 含HomeItem列表的数据Adapter。
  */
-public class HomeFragment extends BaseSwipeRefreshFragment {
+public class HomeFragment extends BaseSwipeRefreshFragment<HomeItem, RecyclerView> {
 
     private static final String TAG = HomeFragment.class.getSimpleName();
 
     private HomeModel homeModel;
 
-    private RecyclerView lv;
     private HomeAdapter cda;
 
-    public HomeFragment() {
+    @Override
+    protected void refreshChangedData(RecyclerView lv, List<HomeItem> dataItems) {
+        Log.d(TAG, "data size " + dataItems.size());
+        cda = new HomeAdapter(getContext(), dataItems, homeModel);
+        lv.setAdapter(cda);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        lv = view.findViewById(R.id.list);
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setViewModel();
+    protected LiveData<List<HomeItem>> setViewModel() {
+        homeModel = newViewModel(HomeModel.class);
         startLoading(homeModel.refreshState);
+        return homeModel.items;
     }
-
 
     @Override
     protected void doLoadingTask() {
         homeModel.load(getTypeface());
     }
 
-    private void setViewModel() {
-        homeModel = getViewModel();
-
-        homeModel.items.observe(this, new Observer<List<HomeItem>>() {
-            @Override
-            public void onChanged(@Nullable List<HomeItem> chartItems) {
-                if (null == chartItems) {
-                    Log.w(TAG, "null data comes.");
-                } else {
-                    Log.d(TAG, "data size " + chartItems.size());
-                    cda = new HomeAdapter(getContext(), chartItems, homeModel);
-                    lv.setAdapter(cda);
-                }
-            }
-        });
-    }
-
-    private HomeModel getViewModel() {
-        return ViewModelProviders.of(this)
-                .get(HomeModel.class);
+    @Override
+    protected int getCollectionLayoutResourceId() {
+        return R.layout.including_recyclerview;
     }
 }
