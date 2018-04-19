35d37179df6ca3280dcde4e32e28bf06d2937e61
yangfeng <yangfeng@sogou-inc.com>
2018-04-19 19:51:41 +0800

    create installed ime app list within backgroud thread via Flowable.fromFeature

--- app/src/main/java/com/harvestasm/apm/add/AddViewModel.java
+++ app/src/main/java/com/harvestasm/apm/add/AddViewModel.java
@@ -7,11 +7,13 @@ import android.util.Log;
 import com.harvestasm.apm.home.HomeDeviceItem;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import io.reactivex.Flowable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 public class AddViewModel extends ViewModel {
     private static final String TAG = AddViewModel.class.getSimpleName();
@@ -20,16 +22,32 @@ public class AddViewModel extends ViewModel {
         return AddDataStorage.get().appListLiveData;
     }
 
+    // Flowable.fromFuture() 在非主线程执行Callable对象
     public void loadImeMethods() {
-        Flowable.just(AddDataStorage.get().createImeAppList())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<HomeDeviceItem.AppItem>>() {
-                    @Override
-                    public void accept(List<HomeDeviceItem.AppItem> appItems) {
-                        Log.e(TAG, "accept thread " + Thread.currentThread().getName());
-                        getImeAppLiveData().setValue(appItems);
-                    }
-                });
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Callable<List<HomeDeviceItem.AppItem>> callable = new Callable<List<HomeDeviceItem.AppItem>>() {
+            @Override
+            public List<HomeDeviceItem.AppItem> call() {
+                return AddDataStorage.get().createImeAppList();
+            }
+        };
+
+        Future<List<HomeDeviceItem.AppItem>> future = executor.submit(callable);
+
+        Consumer<List<HomeDeviceItem.AppItem>> onNext = new Consumer<List<HomeDeviceItem.AppItem>>() {
+            @Override
+            public void accept(List<HomeDeviceItem.AppItem> appItems) {
+                Log.e(TAG, "onNext thread " + Thread.currentThread().getName());
+                getImeAppLiveData().setValue(appItems);
+            }
+        };
+
+//        Flowable.fromCallable(callable).subscribe(onNext);
+        Flowable.fromFuture(future).subscribe(onNext);
+
+//        Flowable.just(AddDataStorage.get().createImeAppList())
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(onNext);
     }
 }
