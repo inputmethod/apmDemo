93b875659e73fd56bbb700b6a94d2ec0e397e0fc
yangfeng <yangfeng@sogou-inc.com>
2018-05-02 15:41:32 +0800

    Show valid data of performance chart.

--- app/src/main/java/com/harvestasm/apm/browser/BrowserViewModel.java
+++ app/src/main/java/com/harvestasm/apm/browser/BrowserViewModel.java
@@ -9,12 +9,6 @@ import android.util.Log;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.ChartData;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.harvestasm.apm.add.AddDataStorage;
 import com.harvestasm.apm.reporter.ApmDataSourceIndex;
@@ -28,13 +22,8 @@ import com.harvestasm.chart.listviewitems.BarChartItem;
 import com.harvestasm.chart.listviewitems.ChartItem;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import typany.apm.agent.android.harvest.ApplicationInformation;
-import typany.apm.agent.android.measurement.CustomMetricMeasurement;
 
 // todo: simplest implement without repository to store data item.
 public class BrowserViewModel extends ViewModel {
@@ -104,24 +93,16 @@ public class BrowserViewModel extends ViewModel {
                 List<String> app = sourceData.getApp();
                 appList.add(app.get(0) + "," + app.get(1));
                 for (ApmMeasurementItem item : sourceData.getMeasurement()) {
-                    buildEntry(entries, (float) item.getMax(), index++);
+                    if (TextUtils.equals(key, item.getName())) {
+                        buildEntry(entries, (float) item.getMax(), index++);
+                    }
                 }
             }
 
             String label = TextUtils.join("|", appList);
-            BarChartItem chartItem = generateDataBar(entries, label, key, typeface);
+            BarChartItem chartItem = generateDataBar(entries, key, label, typeface);
             list.add(chartItem);
         }
-
-        // parse apps
-//        HashMap<String, List<ApmBaseUnit<ApmSourceConnect>>> connectUnits = connectSourceIndex.getAppIndexMap();
-//        ArrayList<BarEntry> entries = new ArrayList<>();
-//        ArrayList<String> keySet = new ArrayList<>(connectUnits.keySet());
-//        for (int i = 0; i < keySet.size(); i++) {
-//            buildEntry(entries, connectUnits.get(keySet.get(i)).size(), i);
-//        }
-//        BarChartItem item = generateDataBar(entries, "版本", "App分布", typeface);
-//        list.add(item);
 
         // final result list.
         onDataLoaded(list);
@@ -129,41 +110,6 @@ public class BrowserViewModel extends ViewModel {
 
     private void buildEntry(ArrayList<BarEntry> entries, float value, int index) {
         entries.add(new BarEntry(index, value));
-    }
-
-    // 缓存的数据视图
-    private List<ChartItem> getDisplayChartItemList(Typeface typeface) {
-        List<ChartItem> list = new ArrayList<>();
-
-        List<ApplicationInformation> appList = new ArrayList<>(AddDataStorage.get().selectedImeAppList); // 统一app排列顺序
-        Map<String, Map<ApplicationInformation, CustomMetricMeasurement>> measurementByOption = AddDataStorage.get().getMeasurementByOption();
-        List<String> optionList = new ArrayList<>(measurementByOption.keySet());
-        Collections.sort(optionList);
-        for (String option : optionList) {
-            Map<ApplicationInformation, CustomMetricMeasurement> measurementMap = measurementByOption.get(option);
-            list.add(generateDataBar(measurementMap, appList, option, typeface));
-        }
-
-        return list;
-    }
-
-    private ChartItem generateDataBar(Map<ApplicationInformation, CustomMetricMeasurement> measurementMap,
-                                      List<ApplicationInformation> appList, String option, Typeface typeface) {
-        assert(appList.size() == measurementMap.size()); // 断言数据与选中的应用总数是一样的
-
-        StringBuilder labelBuilder = new StringBuilder("|");
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        for (int i = 0; i < appList.size(); i++) {
-            ApplicationInformation information = appList.get(i);
-            CustomMetricMeasurement measurement = measurementMap.get(information);
-            buildEntry(entries, (float) measurement.getCustomMetric().getMax(), i);
-            labelBuilder.append(information.getAppName())
-                    .append(information.getAppVersion())
-                    .append("|");
-        }
-
-        BarChartItem item = generateDataBar(entries, option, labelBuilder.toString(), typeface);
-        return item;
     }
 
     private BarChartItem generateDataBar(ArrayList<BarEntry> entries, String label,
@@ -178,32 +124,5 @@ public class BrowserViewModel extends ViewModel {
 
     public void performClick(ChartItem item) {
         clickItem.setValue(item);
-    }
-
-    public void parsePieChartItem(ChartItem item) {
-        ChartData data = item.getChartData();
-        if (data instanceof PieData) {
-            PieData lineData = (PieData) data;
-            List<IPieDataSet> list = lineData.getDataSets();
-            Log.v(TAG, "parseLineChartItem, data set size " + list.size());
-        }
-    }
-
-    public void parseBarChartItem(ChartItem item) {
-        ChartData data = item.getChartData();
-        if (data instanceof BarData) {
-            BarData lineData = (BarData) data;
-            List<IBarDataSet> list = lineData.getDataSets();
-            Log.v(TAG, "parseLineChartItem, data set size " + list.size());
-        }
-    }
-
-    public void parseLineChartItem(ChartItem item) {
-        ChartData data = item.getChartData();
-        if (data instanceof LineData) {
-            LineData lineData = (LineData) data;
-            List<ILineDataSet> list = lineData.getDataSets();
-            Log.v(TAG, "parseLineChartItem, data set size " + list.size());
-        }
     }
 }
