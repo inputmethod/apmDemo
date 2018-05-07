4737a4ec0e33d2e4794982a8b42e3c53ec8bb66e
yangfeng <yangfeng@sogou-inc.com>
2018-05-07 11:45:39 +0800

    comment out field that is duplicate to its super classes.

--- app/src/main/java/com/harvestasm/apm/repository/model/ApmSourceConnect.java
+++ app/src/main/java/com/harvestasm/apm/repository/model/ApmSourceConnect.java
@@ -6,17 +6,17 @@ import com.harvestasm.apm.repository.model.search.ApmBaseSourceType;
 import java.util.List;
 
 public class ApmSourceConnect extends ApmBaseSourceType {
-    private List<String> app;
+//    private List<String> app;
     private List<String> device;
     private List<ApmDeviceMicsItem> devicemics;
 
-    public List<String> getApp() {
-        return app;
-    }
-
-    public void setApp(List<String> app) {
-        this.app = app;
-    }
+//    public List<String> getApp() {
+//        return app;
+//    }
+//
+//    public void setApp(List<String> app) {
+//        this.app = app;
+//    }
 
     public List<String> getDevice() {
         return device;
